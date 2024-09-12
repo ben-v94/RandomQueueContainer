90ddfcb0580ff827ee2da8f2d6d9cf05236e5f5e
ben-v94 <beniaminvyan@gmail.com>
2024-09-13 03:52:27 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+let queue = []
+let head = null
+
+function Enqueue(){
+    if(queue.length == 0) shifts.value = ""
+    queue.push(Math.floor(Math.random() * 201))
+    if(document.querySelector('#flag').checked){
+        queue.sort((x,y) => y - x)
+    }
+    insert()
+
+}
+
+function Dequeue(){
+        head = queue.shift()
+        insert()
+        addShift()
+}
+
+function insert() {
+
+    let i = 0;
+    let elems = document.querySelectorAll('#table th');
+    elems.forEach(function(elem) {
+        if (i < 6) {
+            elem.textContent = queue[i++]
+        } else {
+            elem.textContent = '' 
+        }
+    })    
+}
+
+function addShift(){
+    if (head != undefined)
+    shifts.value+= head + ", "
+}
+
+
